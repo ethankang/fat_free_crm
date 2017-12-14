533014abf2b29a0586ceff18cd261f9bb70840d0
maying <cherry_822@126.com>
2017-12-14 09:21:12 +0800

    工作时间每隔5分钟执行一次

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@ set :output, File.expand_path("../../log/whenever.log", __FILE__)
 
 # 客服人员有未分配的新线索，每5分钟通知销售经理进行分配
 # 销售人员有新的线索没有处理，通知销售人员进行处理
-every 5.minutes do
+every '0,5 9-18 * * *' do
   runner "Lead.ding_sales_manager"
   runner "Lead.ding_sales"
 end
